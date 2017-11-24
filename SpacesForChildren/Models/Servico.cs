4d8dd6ce51dad507fadf66e07f98954856e8ad93
tasguiomar <tiagoalexandresanto@gmail.com>
2017-11-24 10:38:28 +0000

    APAGA LINK

--- SpacesForChildren/Models/Servico.cs
+++ SpacesForChildren/Models/Servico.cs
@@ -21,4 +21,3 @@ namespace SpacesForChildren.Models
     }
 }
 
-https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application
