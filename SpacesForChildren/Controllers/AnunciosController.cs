481c645c31f62457492b69c26af38f7beac9e262
tasguiomar <tiagoalexandresanto@gmail.com>
2017-12-01 23:38:42 +0000

    comi-te anuncios

---
+++ SpacesForChildren/Controllers/AnunciosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SpacesForChildren.Models;
+
+namespace SpacesForChildren.Controllers
+{
+    public class AnunciosController : Controller
+    {
+        private SFCContext db = new SFCContext();
+
+        // GET: Anuncios
+        public ActionResult Index()
+        {
+            var anuncios = db.Anuncios.Include(a => a.Instituicao).Include(a => a.Servico);
+            return View(anuncios.ToList());
+        }
+
+        // GET: Anuncios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anuncio anuncio = db.Anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Create
+        public ActionResult Create()
+        {
+            ViewBag.InstituicaoID = new SelectList(db.Instituicoes, "InstituicaoID", "InstituicaoNome");
+            ViewBag.ServicoID = new SelectList(db.Servicos, "ServicoID", "ServicosDescricao");
+            return View();
+        }
+
+        // POST: Anuncios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AnuncioID,AnuncioTitulo,AnuncioDescricao,AnuncioData,InstituicaoID,ServicoID")] Anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Anuncios.Add(anuncio);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstituicaoID = new SelectList(db.Instituicoes, "InstituicaoID", "InstituicaoNome", anuncio.InstituicaoID);
+            ViewBag.ServicoID = new SelectList(db.Servicos, "ServicoID", "ServicosDescricao", anuncio.ServicoID);
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anuncio anuncio = db.Anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstituicaoID = new SelectList(db.Instituicoes, "InstituicaoID", "InstituicaoNome", anuncio.InstituicaoID);
+            ViewBag.ServicoID = new SelectList(db.Servicos, "ServicoID", "ServicosDescricao", anuncio.ServicoID);
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AnuncioID,AnuncioTitulo,AnuncioDescricao,AnuncioData,InstituicaoID,ServicoID")] Anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(anuncio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstituicaoID = new SelectList(db.Instituicoes, "InstituicaoID", "InstituicaoNome", anuncio.InstituicaoID);
+            ViewBag.ServicoID = new SelectList(db.Servicos, "ServicoID", "ServicosDescricao", anuncio.ServicoID);
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anuncio anuncio = db.Anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Anuncio anuncio = db.Anuncios.Find(id);
+            db.Anuncios.Remove(anuncio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
