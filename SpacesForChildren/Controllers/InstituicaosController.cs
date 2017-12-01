29e927407b0b4088c8f3461266950e36525ac9da
Rafael Almeida <Rafael Almeida@DESKTOP-VOIRGBC>
2017-12-01 13:53:20 +0000

    Views da Instituicao

---
+++ SpacesForChildren/Controllers/InstituicaosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SpacesForChildren.Models;
+
+namespace SpacesForChildren.Controllers
+{
+    public class InstituicaosController : Controller
+    {
+        private SFCContext db = new SFCContext();
+
+        // GET: Instituicaos
+        public ActionResult Index()
+        {
+            return View(db.Instituicoes.ToList());
+        }
+
+        // GET: Instituicaos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instituicao instituicao = db.Instituicoes.Find(id);
+            if (instituicao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instituicao);
+        }
+
+        // GET: Instituicaos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Instituicaos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InstituicaoID,InstituicaoTipo,InstituicaoNome,InstituicaoEmail,InstituicaoTelefone,InstituicaoCidade,InstituicaoMorada")] Instituicao instituicao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Instituicoes.Add(instituicao);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(instituicao);
+        }
+
+        // GET: Instituicaos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instituicao instituicao = db.Instituicoes.Find(id);
+            if (instituicao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instituicao);
+        }
+
+        // POST: Instituicaos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "InstituicaoID,InstituicaoTipo,InstituicaoNome,InstituicaoEmail,InstituicaoTelefone,InstituicaoCidade,InstituicaoMorada")] Instituicao instituicao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instituicao).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(instituicao);
+        }
+
+        // GET: Instituicaos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instituicao instituicao = db.Instituicoes.Find(id);
+            if (instituicao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instituicao);
+        }
+
+        // POST: Instituicaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Instituicao instituicao = db.Instituicoes.Find(id);
+            db.Instituicoes.Remove(instituicao);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
