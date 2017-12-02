372cd6abc394fdbc1c709ce40cdb3c484770b6b6
Rafael Almeida <Rafael Almeida@DESKTOP-VOIRGBC>
2017-12-02 15:07:13 +0000

    Vistas Instituição todas terminadas

--- SpacesForChildren/Models/Instituicao.cs
+++ SpacesForChildren/Models/Instituicao.cs
@@ -14,11 +14,17 @@ namespace SpacesForChildren.Models
 
         [Key]
         public int InstituicaoID { get; set; }
+        [Display(Name = "Tipo: ")]
         public Tipo InstituicaoTipo { get; set; }
+        [Display(Name = "Nome: ")]
         public string InstituicaoNome { get; set; }
+        [Display(Name = "Email: ")]
         public string InstituicaoEmail { get; set; }
+        [Display(Name = "Telefone: ")]
         public int InstituicaoTelefone { get; set; }
+        [Display(Name = "Cidade: ")]
         public string InstituicaoCidade { get; set; }
+        [Display(Name = "Morada: ")]
         public string InstituicaoMorada { get; set; }
 
         public virtual IList<Servico> Servicos { get; set; }
