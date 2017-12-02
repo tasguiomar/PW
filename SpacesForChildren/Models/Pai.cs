bec4bfb9f553515cd8156a00e30e77f4302b2c1a
Rafael Almeida <Rafael Almeida@DESKTOP-VOIRGBC>
2017-12-02 15:57:34 +0000

    Views dos Pais todas criadas

--- SpacesForChildren/Models/Pai.cs
+++ SpacesForChildren/Models/Pai.cs
@@ -10,10 +10,15 @@ namespace SpacesForChildren.Models
     {
         [Key]
         public int PaiID { get; set; }
+        [Display(Name = "Nome: ")]
         public string PaisNome { get; set; }
+        [Display(Name = "CC:  ")]
         public int PaisCc { get; set; }
+        [Display(Name = "NIF: ")]
         public int PaisNif { get; set; }
+        [Display(Name = "Telemóvel")]
         public int PaisTelemovel { get; set; }
+        [Display(Name = "Email")]
         public string PaisEmail { get; set; }
 
         public virtual IList<Pedido> Pedidos { get; set; }
